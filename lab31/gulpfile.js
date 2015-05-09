650e1931e9fbd898d1aeb8d07eff4b6b8b03a879
Carlos Mauricio Idárraga <ing-mauricio@outlook.com>
2015-05-09 07:17:54 -0500

    Clase 11
    - Lab 29 generar package json
    - Lab 30 ejemplo chat
    - Lab 31 ejemplo con gulp permite: Concatenar y minificar js.

---
+++ lab31/gulpfile.js
@@ -0,0 +1,16 @@
+/*
+* Dependencias
+*/
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),// se encarga de concatenar.
+	uglify = require('gulp-uglify');// se encarga de dejar mi archivo en una sola linea de codigo.
+
+/*
+* Configuración de la tarea 'demo'
+*/
+gulp.task('concat-files', function(){
+	gulp.src('source/*.js')
+		.pipe(concat('lab31.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('build/'))
+});
